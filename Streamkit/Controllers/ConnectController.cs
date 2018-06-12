8dfe2319206a588a7c3ab3dcd34118870fe9cdd4
damouryouknow <damourba@gmail.com>
2018-06-11 20:51:01 -0400

    started implementing connect controller

--- Streamkit/Controllers/ConnectController.cs
+++ Streamkit/Controllers/ConnectController.cs
@@ -4,12 +4,31 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
+using Streamkit.Web;
+
 namespace Streamkit.Controllers
 {
     public class ConnectController : Controller
     {
         public IActionResult Index()
         {
+            // TODO: Map OAuth connection links to model.
+            // TODO: Move OAuth logic to OAuth namespace after we get it working.
+
+            // URL params for requesting twitch authentification.
+            // TODO: Read values from credentials config file.
+            UrlParams urlParams = new UrlParams();
+            urlParams.Add("client_id", null);
+            urlParams.Add("redirect_uri", null);
+            urlParams.Add("response_type", "code");
+            urlParams.Add("scope", null);
+            urlParams.Add("force_verify", "true");
+            urlParams.Add("state", null); // TODO: Generate token for this.
+
+            // TODO: ServiceConnectUrl class for storing the connection urls
+            // to all OAuth services.
+            ViewData["service_connect_url"] = null;
+
             return View();
         }
     }
