2c94eea47360f8d6fc34c40050463089c3f6dcc4
damouryouknow <damourba@gmail.com>
2018-07-14 21:14:22 -0400

    pretty colors

---
+++ Streamkit/Core/Gadgets.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Drawing;
+
+namespace Streamkit.Core {
+    public abstract class Gadget {
+        protected string id;
+        protected User user;
+
+        public Gadget(string id, User user) {
+
+        }
+    }
+
+
+    public class Bitbar : Gadget {
+        private int value = 0;
+        private int maxValue = 0;
+        private byte[] image = null;
+        private Color color = Color.Blue;
+
+        public Bitbar(string id, User user) : base(id, user) {
+
+        }
+    }
+}
