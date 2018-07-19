1763846d4dababd1b0acb70ad8399382b1948a30
damouryouknow <damourba@gmail.com>
2018-07-18 20:19:03 -0400

    added twitch client

---
+++ Streamkit/TwitchClient.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using TwitchLib.Client;
+using TwitchLib.Client.Enums;
+using TwitchLib.Client.Events;
+using TwitchLib.Client.Extensions;
+using TwitchLib.Client.Models;
+
+namespace Streamkit.Twitch {
+    public class Bot {
+        public static Bot Instance;
+
+        private TwitchClient client;
+
+        public Bot() {
+            ConnectionCredentials credentials = new ConnectionCredentials(
+                    "BotZura", Config.TwitchChatToken);
+
+            client = new TwitchClient();
+            client.Initialize(credentials);
+
+            client.OnMessageReceived += onMessageReceived;
+            client.OnNewSubscriber += onNewSubscriber;
+            client.OnReSubscriber += onResubsriber;
+            client.OnGiftedSubscription += onGiftedSubscription;
+
+            client.Connect();
+
+            // For testing
+            this.JoinChannel("gogomic");
+
+            Instance = this;
+        }
+
+        public void JoinChannel(string channelName) {
+            this.client.JoinChannel(channelName);
+        }
+
+        private void onMessageReceived(object sender, OnMessageReceivedArgs e) {
+            throw new NotImplementedException();
+        }
+
+        private void onNewSubscriber(object sender, OnNewSubscriberArgs e) {
+            throw new NotImplementedException();
+        }
+
+        private void onResubsriber(object sender, OnReSubscriberArgs e) {
+            throw new NotImplementedException();
+        }
+
+        private void onGiftedSubscription(object sender, OnGiftedSubscriptionArgs e) {
+            throw new NotImplementedException();
+        }
+    }
+}
