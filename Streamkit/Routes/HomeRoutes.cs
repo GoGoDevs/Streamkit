5a67120d247b061ae40ef29763e6d54c0ed7da99
damouryouknow <damourba@gmail.com>
2018-08-05 12:13:21 -0400

    made subscribers look spicer

--- Streamkit/Routes/HomeRoutes.cs
+++ Streamkit/Routes/HomeRoutes.cs
@@ -16,7 +16,7 @@ namespace Streamkit.Routes {
         }
 
         public static IActionResult Subscribers(RequestHandler<IActionResult> req) {
-            User user = UserManager.GetUserTwitch("gogomic");
+            User user = UserManager.GetUserTwitch("damouryouknow");
 
             // TODO: Chain requests if gogomic ever gets over 100 subs.
             string url = "https://api.twitch.tv/kraken/channels/" + user.TwitchId 
